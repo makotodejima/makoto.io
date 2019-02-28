c39340001f06f983291a548ee44e41f351f558a1
Makoto Dejima <makotodejima@gmail.com>
2019-03-01 01:19:24 +0900

    layout adj

--- src/pages/makoto.js
+++ src/pages/makoto.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 
 function IndexPage() {
   return (
@@ -14,17 +13,23 @@ function IndexPage() {
 
       <div className="text-center">
         <h1 className="inline-block my-8 p-3">Makoto Dejima</h1>
-
-        <p className="">
-          Front-end Developer / Graphic Designer{" "}
-          <a
-            href="https://tailwindcss.com/"
-            className="font-bold no-underline text-grey-darkest"
-          >
-            Tailwind
-          </a>
-          , a utility-first CSS framework.
+        <p className="bg-blue-light mb-8">
+          Front-end Developer / Graphic Designer
         </p>
+      </div>
+      <div className="flex flex-wrap justify-around bg-grey-lighter">
+        <div className="w-1/3 h-64 m-4 p-2 bg-grey">
+          <p className="text-lg text-center">1</p>
+        </div>
+        <div className="w-1/3 h-64 m-4 p-2 bg-grey">
+          <p className="text-lg text-center">2</p>
+        </div>
+        <div className="w-1/3 h-64 m-4 p-2 bg-grey">
+          <p className="text-lg text-center">3</p>
+        </div>
+        <div className="w-1/3 h-64 m-4 p-2 bg-grey">
+          <p className="text-lg text-center">4</p>
+        </div>
       </div>
     </Layout>
   );
