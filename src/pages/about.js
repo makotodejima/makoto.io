f46281e7311033706c0122fab2484ef0cab1f36a
Makoto Dejima <makotodejima@gmail.com>
2019-03-01 16:39:06 +0900

    About

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import dogIllustration from "../images/dog-illustration.svg";
+import me from "../images/me.jpg";
 
 function AboutPage() {
   return (
@@ -12,27 +12,35 @@ function AboutPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
       />
 
-      <div className="flex flex-col md:flex-row items-center">
-        <div className="md:w-2/3 md:mr-8">
-          <p className="border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
-            The point is... to live one's life in the full complexity of what one
-            is, which is something much darker, more contradictory, more of a
-            maelstrom of impulses and passions, of cruelty, ecstacy, and madness,
-            than is apparent to the civilized being who glides on the surface and
-            fits smoothly into the world.
-        </p>
+      <div className="flex flex-col items-center">
+        <div className="intro flex flex-col md:flex-row justify-around item-center">
+          <div className="md:w-1/2 md:mr-8">
+            <p className="md:border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
+              私は出島　誠と申します。東京で生まれた後、法律をまなびました。
+              そのごIT企業にはいり広告の営業をやりました。その後、サンフランシスコにわたり、
+              IT系などの主事あとかをしました。そのごベルリンにいきました。
+              いまはデヴべロッパーとグラフィックデザイナーとして働いています。
+            </p>
 
-          <p className="font-bold mt-4 text-right text-xs uppercase">
-            – Thomas Nagel
-        </p>
+            <p className="font-bold mt-4 text-right text-xs uppercase">
+              – Makoto Dejima
+            </p>
+          </div>
+
+          <div className="w-2/3 md:w-1/3">
+            <img src={me} alt="me" />
+          </div>
         </div>
-
-        <div className="w-2/3 md:w-1/3">
-          <img src={dogIllustration} alt="A dog relaxing" />
+        <div className="links flex justify-between w-full bg-blue">
+          <a href="#">Resume</a>
+          <div className="social ">
+            <a href="#">Dribbble</a>
+            <a href="#">Instagram</a>
+          </div>
         </div>
       </div>
     </Layout>
-  )
+  );
 }
 
 export default AboutPage;
