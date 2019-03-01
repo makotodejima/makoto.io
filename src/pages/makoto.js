39181f7c0559229c1fc233ef7e125f7ea477a6f3
Makoto Dejima <makotodejima@gmail.com>
2019-03-02 00:02:04 +0900

    Work page

--- src/pages/makoto.js
+++ src/pages/makoto.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import tpcsq from "../images/tpc-sq.jpg";
 
 function IndexPage() {
   return (
@@ -22,16 +23,17 @@ function IndexPage() {
         <p>View more...</p>
       </div>
       <div className="boxes flex mb-10 flex-wrap justify-around bg-grey-lighter">
-        <div className="box relative w-2/5 h-20rem m-4 p-4 bg-grey">
-          <p className="absolute pin-b bg-grey-darker text-lg text-center">1</p>
+        <div className="box w-2/5 m-4 bg-grey">
+          <img src={tpcsq} alt="Postcard, The light of Tokyo" />
+          <p className="bg-grey-darker text-lg text-center">1</p>
         </div>
-        <div className="box w-2/5 h-20rem m-4 p-4 bg-grey">
+        <div className="box w-2/5 m-4 p-4 bg-grey">
           <p className="text-lg text-center">2</p>
         </div>
-        <div className="box w-2/5 h-20rem m-4 p-4 bg-grey">
+        <div className="box w-2/5 m-4 p-4 bg-grey">
           <p className="text-lg text-center">3</p>
         </div>
-        <div className="box w-2/5 h-20rem m-4 p-4 bg-grey">
+        <div className="box w-2/5 m-4 p-4 bg-grey">
           <p className="text-lg text-center">4</p>
         </div>
       </div>
--- src/pages/work/tokyo-postcard.js
+++ src/pages/work/tokyo-postcard.js
@@ -3,6 +3,11 @@ import React from "react";
 import Layout from "../../components/layout";
 import SEO from "../../components/seo";
 import postcard from "../../images/post-card.jpg";
+import tpc01 from "../../images/tpc01.jpg";
+import tpc02 from "../../images/tpc02.jpg";
+import tpc03 from "../../images/tpc03.jpg";
+import tpc04 from "../../images/tpc04.jpg";
+import tpc05 from "../../images/tpc05.jpg";
 
 function IndexPage() {
   return (
@@ -17,9 +22,12 @@ function IndexPage() {
       </div>
 
       <div className="work flex flex-col">
-        <p className="bg-grey-darker text-lg text-center">1</p>
-        <img src={postcard} />
-        <p>gmlfdkgjaidfsjglsdkajgflkndsnfkls</p>
+        <p className="bg-grey-darker text-lg text-center">Won an award</p>
+        <img src={tpc01} className="my-16 shadow" />
+        <img src={tpc02} className="my-16 shadow" />
+        <img src={tpc03} className="my-16 shadow" />
+        <img src={tpc04} className="my-16 shadow" />
+        <img src={tpc05} className="my-16 shadow" />
       </div>
     </Layout>
   );
