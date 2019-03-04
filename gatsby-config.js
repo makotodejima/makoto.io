0e942ea342be478bc98f8425a0577d8a39e16cf7
Makoto Dejima <makotodejima@gmail.com>
2019-03-04 12:55:46 +0900

    Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,8 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#4dc0b5`,
         display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`
+        icon: `src/images/mdicon.svg`,
+        include_favicon: true
       }
     },
     `gatsby-plugin-postcss`,
