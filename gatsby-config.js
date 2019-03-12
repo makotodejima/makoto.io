d446fabc04951dd333521a2333ac5ca1b63b516e
Makoto Dejima <makotodejima@gmail.com>
2019-03-12 16:29:45 +0900

    Added image using graphQL to 404 page

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,15 @@ module.exports = {
         ]
       }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
     {
       resolve: "gatsby-plugin-purgecss",
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,43 @@
 import React from "react";
 
+import { StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import poco from "../images/poco.png";
+// import poco from "../images/poco.png";
 
-function NotFoundPage() {
-  return (
-    <Layout>
-      <SEO title="404: Not found" />
-      <div>
-        <img
-          src={poco}
-          className="block mx-auto w-1/6"
-          alt="Poco's cute face"
-        />
-        <h2 className="block my-8 p-3 text-center">
-          Looks like you came to wrong place...
-        </h2>
-      </div>
-    </Layout>
-  );
-}
+const NotFoundPage = () => (
+  <StaticQuery
+    query={query}
+    render={data => (
+      <Layout>
+        <SEO title="404: Not found" />
+        <div>
+          <h3 className="block my-4 p-3 text-center">Hi, it's me, Poco.</h3>
+          <Img
+            fluid={data.poco.childImageSharp.fluid}
+            className="block mx-auto w-1/6"
+          />
+          <h2 className="block mt-4 mb-24 p-3 text-center">
+            Looks like you've come to wrong place...
+          </h2>
+        </div>
+      </Layout>
+    )}
+  />
+);
 
 export default NotFoundPage;
+
+const query = graphql`
+  query {
+    poco: file(relativePath: { eq: "poco.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
