18470727acae3cc0e28e1732bba06f56d079d125
Makoto Dejima <makotodejima@gmail.com>
2019-04-17 17:35:33 +0900

    Webkit video tag border-radius fix

--- src/components/AppItem.js
+++ src/components/AppItem.js
@@ -28,6 +28,8 @@ const ImgWrapper = styled.div`
   margin-bottom: 2rem;
   border-radius: 5px;
   overflow: hidden;
+  -webkit-transform: translate3d(0, 0, 0);
+  -moz-transform: translate3d(0, 0, 0);
   box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.1);
   transition: box-shadow 0.2s ease-in;
 
