7c3dec4a83bd0e892b1619cf215c9e19a44279d0
Makoto Dejima <makotodejima@gmail.com>
2019-04-18 20:06:03 +0900

    Spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,7 +64,9 @@ const IndexPage = () => (
         <Label>Apps</Label>
         <Apps />
 
-        <Label>Interested in working together? Get in touch.</Label>
+        <h3 style={{ marginTop: `30px` }}>
+          Interested in working together? Get in touch.
+        </h3>
         <p>
           I am always looking for interesting projects and people to collaborate
           with. Please feel free to get in touch with me. I would love to hear
