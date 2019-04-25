c10725d039027ed9da1a0cb7a4ba3a7a562f746d
Makoto Dejima <makotodejima@gmail.com>
2019-04-26 01:07:21 +0900

    Set square png as ogp image

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import { StaticQuery, graphql } from "gatsby";
-import md from "../images/md.svg";
+import md from "../images/md.png";
 
 function SEO({ description, lang, meta, keywords, title }) {
   return (
