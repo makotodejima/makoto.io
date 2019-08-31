b805f0c831b6dde695b123084fdaa1328955374e
Makoto Dejima <makotodejima@gmail.com>
2019-09-01 00:42:01 +0200

    sb: added sample stories
    fix ts config
    
    sb
    
    Add vid
    
    Add more sb

---
+++ src/__stories__/aboutImage.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import AboutImage from '../components/AboutImage';
+
+storiesOf('AboutImage', module).add('Default', () => <AboutImage />);
