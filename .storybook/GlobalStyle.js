0db8a860c777ff473ef8bb074ebb158adf82fc77
Makoto Dejima <makotodejima@gmail.com>
2019-09-01 21:07:30 +0200

    storybook config

---
+++ .storybook/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+  /* Storybook's view body. Add a bit of white space */
+  body {
+      padding: 20px 10px;
+    }
+  
+`;
+
+export default GlobalStyle;
---
+++ .storybook/config.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { ThemeProvider } from 'styled-components';
+import { light, dark } from '../src/theme/theme';
+import GlobalStyle from './GlobalStyle';
+import '../src/components/layout.scss';
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../src', true, /.stories.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+// Gatsby's Link overrides:
+// Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+};
+// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
+global.__PATH_PREFIX__ = '';
+// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
+window.___navigate = pathname => {
+  action('NavigateTo:')(pathname);
+};
+configure(loadStories, module);
+
+addDecorator(story => (
+  <ThemeProvider theme={light}>
+    <React.Fragment>
+      <GlobalStyle />
+      {story()}
+    </React.Fragment>
+  </ThemeProvider>
+));
