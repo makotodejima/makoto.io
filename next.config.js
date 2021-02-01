6e43d256d452b25fa2bbc58aa56d12f9393b6beb
Makoto Dejima <makotodejima@gmail.com>
2021-02-01 09:34:15 +0100

    chore: gh redirect

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ["www.notion.so"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/gh",
+        destination: "https://github.com/makotodejima",
+        permanent: true,
+      },
+    ];
+  },
 };
